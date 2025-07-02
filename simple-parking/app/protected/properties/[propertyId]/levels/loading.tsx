508b4bc2b4d18ab383bf20529bcbe75ca6943ecf
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-02 15:12:40 -0700

    Add loading stage for property level

---
+++ simple-parking/app/protected/properties/[propertyId]/levels/loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+  <div className="flex w-full items-center justify-center p-10 md:p-10">
+    <div className="w-full max-w-sm">
+            <div className="flex w-full items-center justify-center p-10 md:p-10">
+                <div className="w-full max-w-sm">
+                    Loading property levels...
+                </div>
+            </div>
+    </div>
+</div>)
+}
