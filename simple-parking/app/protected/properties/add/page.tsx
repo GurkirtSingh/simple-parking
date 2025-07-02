68dfcc3370a84b696ff28cf39197a2a21a453aa5
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-01 19:26:02 -0700

    Update add property component name to property form

--- simple-parking/app/protected/properties/add/page.tsx
+++ simple-parking/app/protected/properties/add/page.tsx
@@ -1,10 +1,10 @@
-import { AddProperty } from "@/components/add-property";
+import { PropertyForm } from "@/components/add-property";
 
 export default function Page() {
   return (
     <div className="flex w-full items-center justify-center p-10 md:p-10">
       <div className="w-full max-w-sm">
-        <AddProperty />
+        <PropertyForm />
       </div>
     </div>
   );
