43b6a90f8bab234b42f1f01ea7018d44b255b289
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-03 00:53:03 -0700

    Create add stall route to get input from user and create new stall

---
+++ simple-parking/app/protected/properties/[propertyId]/stalls/add/page.tsx
@@ -0,0 +1,26 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { PropertyStallForm } from "@/components/property/stalls/stall-form";
+
+type PropertyLevelPageProps = {
+  params: {
+    propertyId: string;
+  };
+};
+export default async function Page({ params }: PropertyLevelPageProps) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/auth/login");
+  }
+  const { propertyId } = await params;
+    return (
+    <div className="flex w-full items-start justify-center">
+        {propertyId ? (
+            <PropertyStallForm propertyId={propertyId}/>
+            ):(
+            <p>Property level not found.</p>
+        )}
+    </div>)
+}
