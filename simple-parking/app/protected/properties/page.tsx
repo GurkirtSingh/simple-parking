22e0674dda9da35dac79dacca3b5d0cd347f981f
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-04 09:48:57 -0700

    Modify property page to use property card to display properties. Add greeting message to user by first name

--- simple-parking/app/protected/properties/page.tsx
+++ simple-parking/app/protected/properties/page.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@/lib/supabase/server";
 import { Tables } from "@/lib/supabase/database.types";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Table } from "lucide-react";
+import PropertyCard from "@/components/property/property-card";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -10,6 +12,19 @@ export default async function Page() {
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
     redirect("/auth/login");
+  }
+  let userPropfile: Tables<"user_profiles"> | null = null;
+
+  const {data: propfile, error: profileError} = await supabase
+    .from("user_profiles")
+    .select("*")
+    .eq("id", data.user.id)
+    .maybeSingle();
+
+  if (profileError) {
+    console.error("Error fetching user profile:", profileError);
+  }else if (propfile) {
+    userPropfile = propfile;
   }
 
   let properties: Tables<"properties">[] = [];
@@ -41,29 +56,12 @@ export default async function Page() {
 
   return (
     <div className="flex-1 w-full flex flex-col items-center justify-start gap-12">
-      <h1 className="text-2xl font-bold mb-4">Welcome to your properties!</h1>
+      <h1 className="text-2xl font-bold mb-4">{userPropfile ? `Hello ${userPropfile.first_name}, ` : ""}Welcome to your properties!</h1>
         
       {properties.length > 0 ? (
         <div className="flex flex-col gap-4">
           {properties.map((property) => (
-            <div key={property.id} className="p-4 border rounded-md">
-              <h2 className="text-lg font-semibold">{property.name}</h2>
-              <p>{property.address}</p>
-              <div className="flex gap-2 mt-2">
-                <Button variant="outline" className="mr-2">
-                  <Link href={`/protected/properties/${property.id}/stalls`}>Stalls</Link>
-                </Button>
-                <Button variant="outline" className="mr-2">
-                  <Link href={`/protected/properties/${property.id}/levels`}>Levels</Link>
-                </Button>
-                <Button variant="outline" className="mr-2">
-                  <Link href={`/protected/properties/${property.id}/edit`}>Edit</Link>
-                </Button>
-                <Button variant="destructive" className="mr-2">
-                  <Link href={`/protected/properties/${property.id}/delete`}>Delete</Link>
-                </Button>
-                </div>
-            </div>
+            <PropertyCard key={property.id} property={property} />
           ))}
         </div>
       ):(
