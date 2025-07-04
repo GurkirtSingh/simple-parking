0cbd5f3a19018404abb0fef321c658606b237874
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-04 09:46:34 -0700

    Create property card component to display property's detail

---
+++ simple-parking/components/property/property-card.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Tables } from "@/lib/supabase/database.types";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+
+type PropertyCardProps = {
+    className?: string;
+    property: Tables<"properties">;
+};
+export default async function PropertyCard({property}: PropertyCardProps) {
+
+    return (
+        <div className="flex flex-col gap-4 w-full max-w-sm">
+            <Card className="shadow-md hover:shadow-lg transition-shadow duration-200">
+                <CardHeader>
+                    <CardTitle className="text-lg font-semibold">{property.name}</CardTitle>
+                    <CardDescription className="flex text-sm text-gray-500 gap-1">
+                        {property.address}
+                        </CardDescription>
+                </CardHeader>
+                <CardContent className="flex justify-between items-center">
+                    <div className="flex gap-2">
+                        <Button variant="outline">
+                            <Link href={`/protected/properties/${property.id}/stalls`}>Stalls</Link>
+                        </Button>
+                        <Button variant="outline">
+                            <Link href={`/protected/properties/${property.id}/levels`}>Levels</Link>
+                        </Button>
+                        <Button variant="outline">
+                            <Link href={`/protected/properties/${property.id}/edit`}>Edit</Link>
+                        </Button>
+                        <Button variant="destructive">
+                            <Link href={`/protected/properties/${property.id}/delete`}>Delete</Link>
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}   
