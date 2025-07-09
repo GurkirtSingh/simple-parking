c9ae5be98596d9b01872b2d01cccc6c63d2dc787
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-08 21:27:59 -0700

    Create edit reservation route using reservation form component

---
+++ simple-parking/app/protected/properties/[propertyId]/reservations/[reservationId]/edit/page.tsx
@@ -0,0 +1,35 @@
+import { ReservationFrom } from "@/components/property/reservations/reservation-form";
+import { getReservationById } from '@/app/protected/properties/[propertyId]/reservations/actions';
+
+type PropertyReservationEditPageProps = {
+    params: {
+        propertyId: string;
+        reservationId: string;
+    };
+};
+
+export default async function Page({ params }: PropertyReservationEditPageProps) {
+    const { propertyId, reservationId } = await params;
+    const { data: reservation, error } = await getReservationById(reservationId);
+    if (error) {
+        return (
+            <div className="flex flex-col w-full items-center justify-center text-gray-400">
+                {error}
+            </div>
+        )
+    }
+    if (!reservation) {
+        return (
+            <div className="flex flex-col w-full items-center justify-center text-gray-400">
+                Could Not Found Reservation
+            </div>
+        )
+    }
+    return (
+        <div className="flex w-full items-center justify-center">
+            <div className="w-full max-w-sm">
+                <ReservationFrom propertyId={propertyId} stallId={reservation.stall_id} reservation={reservation} />
+            </div>
+        </div>
+    );
+}
