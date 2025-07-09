80de1ed1cce4e9409f9c76f7670be1935b945281
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-08 21:29:29 -0700

    Create delete reservation route using reservation delete form component

---
+++ simple-parking/app/protected/properties/[propertyId]/reservations/[reservationId]/delete/page.tsx
@@ -0,0 +1,25 @@
+import { getReservationById } from '@/app/protected/properties/[propertyId]/reservations/actions'
+import ReservationDeleteForm from "@/components/property/reservations/reservation-delete-form"
+
+interface DeletePageProps {
+    params: { 
+        propertyId: string;
+        reservationId: string;
+    }
+}
+
+export default async function Page({ params }: DeletePageProps) {
+    const {propertyId, reservationId} = await params
+    const { data: reservation } = await getReservationById(reservationId)
+    if (!reservation) {
+        return <>Could Not Found Reservation</>
+    }
+
+    return (
+        <div className="max-w-md mx-auto mt-20 space-y-4 text-center">
+            <h2 className="text-xl font-semibold">Confirm Delete</h2>
+            <p>Are you sure you want to delete the reservation for <strong>{reservation.id}</strong> ?</p>
+            <ReservationDeleteForm propertyId={propertyId} reservationId={reservationId}/>
+        </div>
+    )
+}
