990d08525d04b74e3036e06eb7db680a1e6d64c4
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-08 21:24:23 -0700

    Add reservations route which show reservation list component and toasts

---
+++ simple-parking/app/protected/properties/[propertyId]/reservations/page.tsx
@@ -0,0 +1,46 @@
+import { getPropertyReservations } from '@/app/protected/properties/[propertyId]/reservations/actions';
+import { ReservationList } from '@/components/property/reservations/reservation-list';
+import { Tables } from '@/lib/supabase/database.types';
+import { getUser } from '@/lib/supabase/property';
+import { ReservationDeletedToast, ReservationCreatedToast } from '@/components/property/reservations/reservation-toast';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+type PropertyReservationsPageProps = {
+    params: {
+        propertyId: string;
+    };
+};
+
+export default async function Page({ params }: PropertyReservationsPageProps) {
+
+    const { propertyId } = await params;
+    let property: Tables<'properties'> | null = null;
+    let reservations: Tables<'reservations'>[] = [];
+    try {
+        const user = await getUser();
+        reservations = await getPropertyReservations({ property_id: propertyId });
+    } catch (error) {
+        console.error("Error fetching property or reservations:", error);
+        return <p>Error fetching property or reservations.</p>;
+    }
+    return (
+        <div className="flex-1 w-full flex flex-col items-center justify-start">
+            <ReservationDeletedToast />
+            <ReservationCreatedToast />
+            {reservations.length > 0 ? (
+                <ReservationList propertyId={propertyId} reservations={reservations} />
+            ) :
+                <div className='flex w-full flex-col items-center justify-center'>
+                    There are no reservation for this property yet.
+                </div>}
+            <div className="text-center text-gray-500">
+                <div>
+                    <Button variant="outline" className="mt-4">
+                        <Link href={`/protected/properties/${propertyId}/reservations/add`}>Add Reservation</Link>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
