e2b3478039f8465e65d893bd4f0833dd2690b89a
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-14 23:39:13 -0700

    Add dashboard toast to feedback user of checkout status

---
+++ simple-parking/components/dashboard/toast.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { toast } from 'sonner'
+
+
+export default function DashboardToast() {
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    useEffect(() => {
+        const checkout = searchParams.get('checkout')
+        if (checkout === '1') {
+            toast.success('Checked out successfully')
+        }
+        if (checkout === '0') {
+            toast.error('Could Not Check out')
+        }
+        if (checkout) {
+            const newParams = new URLSearchParams(searchParams)
+            newParams.delete('checkout')
+            router.replace(`/protected/dashboard?${newParams.toString()}`)
+        }
+
+    }, [searchParams])
+    return null
+}
