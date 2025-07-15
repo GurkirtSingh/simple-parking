221982382844b5bb27b42df7cec366a20a09c7ac
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-14 23:46:17 -0700

    Modify dashboard page to fetch user properties and display in tabs and show

--- simple-parking/app/protected/dashboard/page.tsx
+++ simple-parking/app/protected/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import DashboardToast from "@/components/dashboard/toast";
+import PropertyTabs from "@/components/dashboard/property-tabs";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -9,7 +13,7 @@ export default async function Page() {
   if (error || !data?.user) {
     redirect("/auth/login");
   }
-  
+
   // Fetch user profile data to greet the user
   // If no profile is found, redirect to setup profile page
   let first_name = "";
@@ -28,11 +32,34 @@ export default async function Page() {
     }
     first_name = profile?.first_name || "";
   }
+  // fetch user assigned properties
+  const { data: userProperties, error:userPropertieserror } = await supabase
+    .from("user_properties")
+    .select("*, properties(name)")
+    .eq("user_id", data.user.id)
+
+  if (userPropertieserror) {
+    console.error(userProperties)
+  }
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        Hello {first_name} welcome to your dashboard!
-      </div>
+    <div className="flex w-full flex-col gap-12">
+      <DashboardToast/>
+      {userProperties && userProperties.length > 0 ? (
+        <PropertyTabs userProperties={userProperties ?? []}/>
+      ) : (
+        <div className="text-center text-gray-500">
+          You have no properties yet. Start by adding a property.
+          <div className="text-center text-gray-500">
+            <div>
+              <Button variant="outline" className="mt-4">
+                <Link href="/protected/properties/add">Add Property</Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+
+
+      )}
     </div>
   );
 }
