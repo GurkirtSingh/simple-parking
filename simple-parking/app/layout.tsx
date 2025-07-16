0b7433e8cea92c380f4f2181a5408131264c8b3b
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-15 23:23:14 -0700

    Add favicon, android-chrome and apple-touch icons and update metadata config

--- simple-parking/app/layout.tsx
+++ simple-parking/app/layout.tsx
@@ -12,6 +12,17 @@ export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Simple Parking",
   description: "A simple parking management system",
+  icons: {
+    icon: [
+      { url:"/favicon.ico", type: "image/x-icon"},
+      { url:"/favicon-16x16", type: "image/png", sizes:"16x16"},
+      { url:"/favicon-32x32", type: "image/png", sizes:"32x32"},
+      { url: "/android-chrome-192x192.png", type: "image/png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", type: "image/png", sizes: "512x512" }
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest"
 };
 
 const geistSans = Geist({
