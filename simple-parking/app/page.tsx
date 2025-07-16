13e5c1f1cee61d8c024fc8f674890f41c91e4eaf
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-15 23:27:29 -0700

    Update landing page to use new logo component

--- simple-parking/app/page.tsx
+++ simple-parking/app/page.tsx
@@ -5,6 +5,7 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { SimpleParkingLogo } from "@/components/SimpleParkingLogo";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Simple Parking</Link>
+              <SimpleParkingLogo className="text-lg" />
             </div>
             <div className="flex items-center gap-5">
               {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
@@ -26,7 +27,8 @@ export default function Home() {
             <Link href="/protected/dashboard">Go to Dashboard</Link>
           </Button>
         </div>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+        <div className="flex-1 flex flex-col max-w-5xl p-5">
+          <SimpleParkingLogo className="text-6xl text-center"/>
           <Hero />
         </div>
 
