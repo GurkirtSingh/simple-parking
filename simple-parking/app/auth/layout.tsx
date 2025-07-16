7c944f09086dfec385e8d24e350a7a1f0f3c6871
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-15 23:40:29 -0700

    remove unused 'hero' component

--- simple-parking/app/auth/layout.tsx
+++ simple-parking/app/auth/layout.tsx
@@ -1,6 +1,5 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
