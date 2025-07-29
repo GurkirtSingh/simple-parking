61a3f46d893a348b50c69201a6d80e77ecffe278
GurkirtSingh <gurkirtsingh143@gmail.com>
2025-07-28 19:24:11 -0700

    fix: make top navigation bar sticky using fixed positioning

--- simple-parking/app/protected/layout.tsx
+++ simple-parking/app/protected/layout.tsx
@@ -14,7 +14,7 @@ export default function ProtectedLayout({
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-10 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <nav className="fixed top-0 shadow bg-background w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-6xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <SimpleParkingLogo className="text-lg" />
@@ -25,7 +25,7 @@ export default function ProtectedLayout({
             </div>
           </div>
         </nav>
-        <div className="flex-1 flex flex-col w-full lg:max-w-6xl px-2 gap-10">
+        <div className="flex-1 flex flex-col w-full lg:max-w-6xl px-2 gap-10 pt-20">
           <div className="flex justify-start gap-2">
             <DashboardButton />
             <ManagePropertiesButton />
